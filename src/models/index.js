8603b318050bcea6cdc344ad875f5e906b630a13
HaninHaidrah <haneenhadey@gmail.com>
2022-03-19 22:02:44 +0300

    backend

---
+++ src/models/index.js
@@ -0,0 +1,32 @@
+"use strict";
+require("dotenv").config();
+
+const userModel = require("./users.js");
+const likedPhotos = require("../models/likedPhotos");
+const Collection = require("../models/data-collection");
+
+const { Sequelize, DataTypes } = require("sequelize");
+
+const DATABASE_URL = "postgres://localhost:5432/gallery";
+// process.env.NODE_ENV === "test" ? "sqlite:memory:" : process.env.DATABASE_URL;
+
+let sequelizeOptions =
+  process.env.NODE_ENV === "production"
+    ? {
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        },
+      }
+    : {};
+
+const sequelize = new Sequelize(DATABASE_URL, sequelizeOptions);
+const liked = likedPhotos(sequelize, DataTypes);
+
+module.exports = {
+  db: sequelize,
+  users: userModel(sequelize, DataTypes),
+  liked: new Collection(liked),
+};
---
+++ src/server.js
@@ -0,0 +1,44 @@
+"use strict";
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+const PORT = process.env.PORT || 5648;
+
+const authRouter = require("./routes/auth.routes");
+const V2Router = require("./routes/v2.route");
+const notFoundHandler = require("./error-handlers/404.js");
+const errorHandler = require("./error-handlers/500.js");
+
+const cors = require("cors");
+const morgan = require("morgan");
+
+app.use(cors());
+app.use(morgan("dev"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(authRouter);
+app.use("/api/v2", V2Router);
+
+app.get("/erro", (req, res) => {
+  throw new Error("Error");
+});
+app.use(errorHandler);
+
+////////////////////////////////////////////////////////////////
+
+function start() {
+  app.listen(PORT, () => {
+    console.log(`Server on ${PORT}`);
+  });
+}
+
+module.exports = {
+  start,
+  app,
+};
